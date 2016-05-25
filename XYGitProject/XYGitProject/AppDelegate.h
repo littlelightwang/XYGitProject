ae58c75bde8ec5853e08cc50d9375ff400d26695
王小亮 <wangxiaoliang@vansky.cn>
2016-05-25 17:17:01 +0800

    add project

---
+++ XYGitProject/XYGitProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XYGitProject
+//
+//  Created by 王小亮 on 16/5/25.
+//  Copyright © 2016年 王小亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XYGitProject/XYGitProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XYGitProject
+//
+//  Created by 王小亮 on 16/5/25.
+//  Copyright © 2016年 王小亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
